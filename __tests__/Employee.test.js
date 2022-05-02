43a6b44b35f0ab06c737cafbc5d33129b8214bc6
VMLujanJr <vmlujanjr@outlook.com>
2022-05-01 22:20:10 -0500

    implemented getRole method

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,38 +8,38 @@ const Employee = require('../lib/assets/js/Employee.js');
 // ============
 test('create an Employee object', () => {
     // create a new Employee instance
-    const employee = new Employee('Victor', 'vmlujanjr@outlook.com', '123456789012');
+    const employee = new Employee('Victor', 'vmlujanjr@outlook.com', '123456789012', 'Employee');
 
     // expect Employee to contain the following properties
     expect(employee.name).toBe('Victor');
     expect(employee.email).toBe('vmlujanjr@outlook.com');
     expect(employee.id).toBe('123456789012');
+    expect(employee.role).toBe('Employee');
 });
 
 test('get Employee name', () => {
     // create a new Employee instance
-    const employee = new Employee('Victor', 'vmlujanjr@outlook.com', '123456789012');
+    const employee = new Employee('Victor', 'vmlujanjr@outlook.com', '123456789012', 'Employee');
 
     expect(employee.getName()).toEqual(expect.any(String));
 });
 
 test('get Employee id', () => {
-    // create a new Employee instance
-    const employee = new Employee('Victor', 'vmlujanjr@outlook.com', '123456789012');
-
+       // create a new Employee instance
+       const employee = new Employee('Victor', 'vmlujanjr@outlook.com', '123456789012', 'Employee');
     expect(employee.getId()).toEqual(expect.any(String));
 });
 
 test('get Employee email', () => {
     // create a new Employee instance
-    const employee = new Employee('Victor', 'vmlujanjr@outlook.com', '123456789012');
+    const employee = new Employee('Victor', 'vmlujanjr@outlook.com', '123456789012', 'Employee');
 
     expect(employee.getEmail()).toEqual(expect.any(String));
 });
 
 test('get Employee role', () => {
     // create a new Employee instance
-    const employee = new Employee('Victor', 'vmlujanjr@outlook.com', '123456789012');
+    const employee = new Employee('Victor', 'vmlujanjr@outlook.com', '123456789012', 'Employee');
 
     expect(employee.getRole()).toEqual(expect.any(String));
 });
